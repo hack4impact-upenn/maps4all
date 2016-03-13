86a66b18e050fbb2b066c99ad19d420a6589b78f
Veronica Wharton <wharton.veronica@gmail.com>
2016-03-13 03:20:21 -0400

    Added basic CSV parsing

--- app/bulk/views.py
+++ app/bulk/views.py
@@ -1,4 +1,4 @@
-from flask import flash, redirect, render_template, url_for
+from flask import redirect, render_template, url_for
 from flask.ext.login import (
     login_required
 )
@@ -6,6 +6,7 @@ from . import bulk
 from forms import (
     UploadForm
 )
+import csv
 
 
 @bulk.route('/upload', methods=['GET', 'POST'])
@@ -14,9 +15,13 @@ def upload():
     """Upload new resources in bulk with CSV file."""
     form = UploadForm()
     if form.validate_on_submit():
-        csv = form.csv.data
-        print csv
-        flash('File successfully uploaded', 'form-success')
+        file = form.csv.data
+        # Read CSV file line-by-line.
+        # Stand-in for future CSV parsing.
+        with file.stream as csvfile:
+            reader = csv.reader(csvfile)
+            for row in reader:
+                print ', '.join(row)
         return redirect(url_for('bulk.review'))
     return render_template('bulk/upload.html', form=form)
 
