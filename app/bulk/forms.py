13c32f60954478f261c28c53166ff98faca89b57
Veronica Wharton <wharton.veronica@gmail.com>
2016-03-14 16:52:38 -0400

    Fixed import

--- app/bulk/forms.py
+++ app/bulk/forms.py
@@ -1,8 +1,6 @@
 from flask.ext.wtf import Form
 from flask_wtf.file import FileAllowed, FileField, FileRequired
-from wtforms.fields import (
-    SubmitField
-)
+from wtforms.fields import SubmitField
 
 
 class UploadForm(Form):
