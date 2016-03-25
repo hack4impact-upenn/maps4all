ed8c6bdf2969d7017aca82ac7a704d6a8163acfb
Elizabeth Hamp <ehamp@seas.upenn.edu>
2016-03-25 14:14:01 -0400

    loaded pins onto the map with a mouse over info window

--- app/assets/scripts/vendor/map.js
+++ app/assets/scripts/vendor/map.js
@@ -4,7 +4,7 @@
 
       function initMap() {
         var map = new google.maps.Map(document.getElementById('map'), {
-          center: {lat: -33.8688, lng: 151.2195},
+          center: {lat: 39.949, lng: -75.181},
           zoom: 13
         });
         var input = /** @type {!HTMLInputElement} */(
@@ -74,4 +74,35 @@
         setupClickListener('changetype-all', []);
         setupClickListener('changetype-address', ['address']);
         setupClickListener('changetype-establishment', ['establishment']);
+
+        $.ajax({
+          type: "GET",
+          url: "/get-resource"
+        }).done(function(data){
+            data = JSON.parse(data)
+            for(var i = 0; i < data.length; i++){
+            //we have the pins data, access it here and place pins on map
+              latLng = new google.maps.LatLng(data[i].Latitude, data[i]
+              .Longitude)
+              var marker = new google.maps.Marker({
+                position: latLng,
+                map: map,
+                visible: true,
+                title: data[i].Name
+              });
+
+              var infowindow = new google.maps.InfoWindow({
+                content: 'Have to fix the content with variable shadowing'
+              });
+
+              marker.addListener('mouseover', function() {
+                infowindow.open(map, this);
+              });
+              marker.addListener('mouseout', function() {
+                infowindow.close();
+              });
+              marker.setMap(map);
+             }
+         })
       }
+
--- app/main/views.py
+++ app/main/views.py
@@ -1,7 +1,21 @@
 from flask import render_template
 from . import main
+from ..models import Resource
+import json
+from flask import jsonify
 
-
-@main.route('/')
+@main.route('/', methods=['GET'])
 def index():
     return render_template('main/index.html')
+
+@main.route('/get-resource', methods=['GET'])
+def getResource():
+    Resource.generate_fake()
+    pins = Resource.query.all()
+    data = []
+    for pin in pins:
+       this_pin = {'Name': pin.name, 'Latitude': pin.latitude, 'Longitude':
+           pin.longitude}
+       data.append(this_pin)
+    print data
+    return json.dumps(data)
