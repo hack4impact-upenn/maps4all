84029d75e20b623f10fde3c923f6815060baecd5
Arman Tokanov <armatoka@gmail.com>
2016-04-28 17:27:01 -0400

    Add static pages support.

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -4,6 +4,7 @@ module (as opposed to just their python files)
 """
 
 from bulk import *  # flake8: noqa
+from editable import *  # flake8: noqa
 from resource import * # flake8: noqa
-from user import *  # flake8: noqa
 from suggestion import * # flake8: noqa
+from user import *  # flake8: noqa
