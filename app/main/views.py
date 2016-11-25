9e883aea2ad8422c69cf9deb024dc75457504b33
Stephanie Shi <stephanie.y.shi@gmail.com>
2016-11-25 13:37:55 -0800

    more code style changes

--- app/main/views.py
+++ app/main/views.py
@@ -13,7 +13,6 @@ from datetime import datetime
 def index():
     return render_template('main/index.html')
 
-
 @main.route('/get-resources')
 def get_resources():
     resources = Resource.query.all()
@@ -38,13 +37,11 @@ def get_associations(resource_id):
         associations[od.descriptor.name] = od.descriptor.values[od.option]
     return json.dumps(associations)
 
-
 @main.route('/about')
 def about():
     editable_html_obj = EditableHTML.get_editable_html('about')
     return render_template('main/about.html',
                            editable_html_obj=editable_html_obj)
-
 
 @main.route('/contact')
 def contact():
@@ -52,24 +49,19 @@ def contact():
     return render_template('main/contact.html',
                            editable_html_obj=editable_html_obj)
 
-
 @main.route('/update-editor-contents', methods=['POST'])
 @login_required
 def update_editor_contents():
     """Update the contents of an editor."""
-
     edit_data = request.form.get('edit_data')
     editor_name = request.form.get('editor_name')
-
     editor_contents = EditableHTML.query.filter_by(
         editor_name=editor_name).first()
     if editor_contents is None:
         editor_contents = EditableHTML(editor_name=editor_name)
     editor_contents.value = edit_data
-
     db.session.add(editor_contents)
     db.session.commit()
-
     return 'OK', 200
 
 @csrf.exempt
@@ -86,7 +78,8 @@ def post_rating():
                 db.session.add(rating)
                 db.session.commit()
             elif star_rating:
-                rating = Rating(rating = star_rating)
+                rating = Rating(submission_time=time,
+                                rating=star_rating)
                 db.session.add(rating)
                 db.session.commit()
     return jsonify(status='success')
