70e45ebf9f066a8ac28a7e8acf5cf6140a4cbb51
sanjayss34 <sanjay_ssubramanian@hotmail.com>
2016-11-27 14:37:36 -0600

    Filter by req-opt-desc works on admin resources view

--- app/assets/scripts/resources.js
+++ app/assets/scripts/resources.js
@@ -2,9 +2,13 @@ $(document).ready(function () {
     // This is for search on the index page
     $('#search-resources').submit(function(e) {
         e.preventDefault();
-        console.log(document.getElementById('search-resources-options').value);
-        var query = 'name=' + document.getElementById('search-resources-text').value + '&' + 'option=' + document.getElementById('search-resources-options').value;
-        var endpoint = '/single-resource/' + query;
-        //window.location.replace(endpoint);
+        console.log(document.querySelector('select').selectedOptions);
+        var query = '?name=' + document.getElementById('search-resources-text').value;
+        var options = document.querySelector('select').selectedOptions;
+        for (var i = 0; i < options.length; i++) {
+            query += '&' + 'option=' + options[i].label;
+        }
+        var endpoint = '/single-resource/search' + query;
+        window.location.replace(endpoint);
     });
 });
--- app/single_resource/views.py
+++ app/single_resource/views.py
@@ -24,19 +24,35 @@ def index():
         options = req_opt_desc.values
     return render_template('single_resource/index.html', resources=resources, options=options)
 
-@single_resource.route('/<query>')
+@single_resource.route('/search')
 @login_required
-def search_resources(query):
+def search_resources():
     name = request.args.get('name')
-    options = request.args.get('option')
-    resource_pool = Resource.query.filter(Resource.name.contains(name))
+    options = request.args.getlist('option')
+    resource_pool = Resource.query.filter(Resource.name.contains(name)).all()
     req_opt_desc = RequiredOptionDescriptor.query.all()[0]
-    for resource in resource_pool:
-        associations = OptionAssociation.query.filter_by(
-            resource_id=resource.id,
-            descriptor_id=req_opt_desc.id
-        )
-    return render_template('single_resource/index.html', resources=resources, query_name=query_name, query_option=query_option)
+    req_opt_desc = Descriptor.query.filter_by(
+        id=req_opt_desc.descriptor_id
+    ).first()
+    resources = list(resource_pool)
+    if req_opt_desc is not None and len(options) > 0:
+        resources = []
+        int_options = []
+        for o in options:
+            int_options.append(req_opt_desc.values.index(str(o)))
+        for resource in resource_pool:
+            associations = OptionAssociation.query.filter_by(
+                resource_id=resource.id,
+                descriptor_id=req_opt_desc.id
+            )
+            for a in associations:
+                if a.option in int_options:
+                    resources.append(resource)
+                    break
+    options = []
+    if req_opt_desc is not None:
+        options = req_opt_desc.values
+    return render_template('single_resource/index.html', resources=resources, query_name=name, options=options)
 
 @single_resource.route('/create', methods=['GET', 'POST'])
 @login_required
