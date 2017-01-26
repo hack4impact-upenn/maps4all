68b33f8ab1a056f83919f7eafb0ec84890d3b2c1
riyer15 <rani.iyer15@gmail.com>
2017-01-25 23:36:23 -0500

    add twilio information

--- app/main/views.py
+++ app/main/views.py
@@ -1,5 +1,6 @@
 import json
-from twilio.rest import TwilioLookupsClient, TwilioRestClient 
+from twilio.rest.lookups import TwilioLookupsClient
+from twilio.rest import TwilioRestClient 
 from flask import render_template, url_for, request, jsonify
 from flask.ext.login import login_required
 from twilio import twiml
